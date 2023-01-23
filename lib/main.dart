3cef62d9eda4f915168e4231a6cab6ca8b2204cb
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-01-23 18:58:42 +0100

    Changes to be committed:

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,20 +8,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: SafeArea(
-            child: Column(
-      children: <Widget>[
-        CircleAvatar(
-          radius: 50.0,
-          backgroundImage: AssetImage('images/nftDp.jpg'),
-          child: Text(
-            'Paul Fidelis',
-            style: TextStyle(
-              fontSize: 40,
-            ),
-          ),
-        )
-      ],
-    )));
+        home: Scaffold(
+            backgroundColor: Colors.teal,
+            body: SafeArea(
+                child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/nftDp.jpg'),
+                  child: Text(
+                    'Paul Fidelis',
+                    style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Pacifico-Regular'),
+                  ),
+                ),
+                Text('Flutter Developer',
+                    style: TextStyle(
+                      fontFamily: 'SourceSansPro-Regular',
+                      color: Colors.teal[100],
+                      fontSize: 20.0,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ],
+            ))));
   }
 }
